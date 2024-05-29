a74d2ec5c114718fcf150a7b9c26ed425a19af2b
phamanhtan <phamanhtan@proton.me>
2024-05-29 12:33:52 +0700

    remove script build

--- script.sh
+++
@@ -1,3 +0,0 @@
-echo "" > ./node_modules/@injectivelabs/sdk-ts/dist/esm/core/utils/Denom/DenomClient.d.ts
-echo "" > ./node_modules/@injectivelabs/sdk-ts/dist/esm/core/utils/Denom/DenomClient.js
-echo "" > ./node_modules/@injectivelabs/sdk-ts/dist/esm/core/utils/Denom/DenomClient.js
