3611de8a427aaa97ad8255a8fca8dd33fb397f69
phamanhtan <phamanhtan@proton.me>
2024-05-29 13:08:05 +0700

    add metadata

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '',
-    baseUrl: '',
+    basePath: '/',
+    baseUrl: 'connecto.thecosmicblock.com',
     images: {
       remotePatterns: [
         {
